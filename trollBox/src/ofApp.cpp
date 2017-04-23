2a64f1023c8c3b0d21a2acd90732aac962242797
Galliez Bruno <galliez.bruno@gmail.com>
2017-04-23 16:57:59 +0200

    modification des touches pour les boutons trollbox

--- trollBox/src/ofApp.cpp
+++ trollBox/src/ofApp.cpp
@@ -23,7 +23,7 @@ void ofApp::setup(){
 	6 = Bouton milieu gauche
 	13 = Bouton Milieu Droit
 	19 = Bouton Droit
-	26 = Detecteur de piÃ¨ce
+	26 = Detecteur de piece
 	*/
 	wiringPiSetupGpio();
 	pinMode(5,INPUT);
@@ -40,19 +40,140 @@ void ofApp::update(){
 	// empêche les détections à moind d' 1 par 300 ms
 	if ( timerDetection+300 < ofGetElapsedTimeMillis() ){
 
-		ofLog() << digitalRead(5) << " " << digitalRead(6) << " " << digitalRead(13) << " " << digitalRead(19) << " " << digitalRead(26);
+		timerDetection = ofGetElapsedTimeMillis();
 
 		/********************************* INSERTION PIECE ********************************/
 		if ( menuPrincipal.etatMenu < 3 ){
 
 			// 1 par défaut, détection  = 0
-			if ( digitalRead(37) == 0 ){
+			if ( digitalRead(26) == 0 ){
 				myPlayer.playSound("sucess01");
 				menuPrincipal.etatMenu++;
 			}
 
 		}
-		timerDetection = ofGetElapsedTimeMillis();
+
+		/****************************** JEUX 0 BOURRINNAGE *********************************************/
+		if ( mesJeux.choixGame == 0 ){
+
+			// charge la force du marteau s'il reste du temps
+			if ( mesJeux.tpsTimerAnimHammer > 0 ){
+				mesJeux.loadHammer();
+			}
+
+
+
+			/****************************** CONTROLE LES LETTRE POUR INDIQUER SON NOM *********************************************/
+		} else if ( menuPrincipal.etatMenu == 3 ){
+
+			// Decale a gauche
+			if ( digitalRead(5) == 0 ){
+				menuPrincipal.changeNameRight();
+				// decale a droite
+			} else if ( digitalRead(6) == 0 ){
+				menuPrincipal.changeLetter(false);
+				// changer la lettre ( ++ )
+			} else if ( digitalRead(13) == 0 ){
+				menuPrincipal.changeLetter(true);
+				// valide le nom
+			} else if ( digitalRead(19) == 0 ){
+				menuPrincipal.etatMenu = 4;
+			}
+
+			/****************************** LOADING BAR *********************************************/
+		}  else if ( menuPrincipal.etatMenu == 5 ){
+
+			bool a=false,b=false,c=false,d=false;
+
+			// bouton 1
+			if ( digitalRead(5) == 0 ){
+				a=true;
+				// bouton 2
+			} else if ( digitalRead(6) == 0 ){
+				b=true;
+				// bouton 3
+			} else if ( digitalRead(13) == 0 ){
+				c=true;
+				// bouton 4
+			} else if ( digitalRead(19) == 0 ){
+				d=true;
+			}
+
+			menuPrincipal.newModificateur(a,b,c,d);
+			/****************************** CHOIX MENU *********************************************/
+		} else if ( menuPrincipal.etatMenu == 6 ){
+
+			// Decale a gauche
+			if ( digitalRead(5) == 0 ){
+				menuPrincipal.changeChoixMenu(false);
+				// decale a droite
+			} else if ( digitalRead(6) == 0 ){
+				menuPrincipal.changeChoixMenu(true);
+				// changer la lettre ( ++ )
+			} else if ( digitalRead(13) == 0 || digitalRead(19) == 0 ){
+				menuPrincipal.valideChoixMenu();
+			}
+
+			/****************************** PASSWORD ADMINISTRATION *********************************************/
+		} else if ( menuPrincipal.etatMenu == 8 ){
+			// bouton 1
+			if ( digitalRead(5) == 0 ){
+				menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 1;
+				// bouton 2
+			} else if ( digitalRead(6) == 0 ){
+				menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 2;
+				// bouton 3
+			} else if ( digitalRead(13) == 0 ){
+				menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 3;
+				// bouton 4
+			} else if ( digitalRead(19) == 0 ){
+				menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 4;
+			}
+			menuPrincipal.indexSequence--;
+			menuPrincipal.decoupeXBomb01++;
+			if ( menuPrincipal.decoupeXBomb01 > 8 ){ menuPrincipal.decoupeXBomb01 = 8;}
+
+
+			/****************************** CREDITS *********************************************/
+		} else if ( menuPrincipal.etatMenu == 9 ){
+
+			menuPrincipal.etatEtapeCredits++;
+			if ( menuPrincipal.etatEtapeCredits == 9 ){ 
+				myPlayer.stopSound();
+				menuPrincipal.etatMenu = 0;
+			}
+
+			/****************************** ADMINISTRATION *********************************************/
+		} else if ( menuPrincipal.etatMenu == 10 ){
+
+			// bouton 1
+			if ( digitalRead(5) == 0 ){
+				if ( myPlayer.soundVolume < 0.04f ){
+					myPlayer.playSound("fail01",true);
+				} else {
+					myPlayer.soundVolume-=0.05f;
+				}
+				// bouton 2
+			} else if ( digitalRead(6) == 0 ){
+				menuPrincipal.optionSelectionAdminsitration--;
+				if ( menuPrincipal.optionSelectionAdminsitration < 0 ){ menuPrincipal.optionSelectionAdminsitration = 0 ;}
+				// bouton 3
+			} else if ( digitalRead(13) == 0 ){
+				menuPrincipal.optionSelectionAdminsitration++;
+				menuPrincipal.etatMenu=6;
+				// bouton 4
+			} else if ( digitalRead(19) == 0 ){
+				if ( myPlayer.soundVolume > 0.96f ){
+					myPlayer.playSound("fail01",true);
+				} else {
+					myPlayer.soundVolume+=0.05f;
+				}
+
+			}
+
+
+		}
+
 	}
 
 	// si un jeu se fini, on renvoi au démarrage
@@ -90,126 +211,6 @@ void ofApp::keyReleased(int key){
 		exit();
 	}
 
-	/****************************** JEUX 0 BOURRINNAGE *********************************************/
-	if ( mesJeux.choixGame == 0 ){
-
-		// charge la force du marteau s'il reste du temps
-		if ( mesJeux.tpsTimerAnimHammer > 0 ){
-			mesJeux.loadHammer();
-		}
-
-
-
-	/****************************** CONTROLE LES LETTRE POUR INDIQUER SON NOM *********************************************/
-	} else if ( menuPrincipal.etatMenu == 3 ){
-
-		// Decale a gauche
-		if ( key == OF_KEY_LEFT ){
-			menuPrincipal.changeNameRight();
-		// decale a droite
-		} else if ( key == OF_KEY_DOWN ){
-			menuPrincipal.changeLetter(false);
-		// changer la lettre ( ++ )
-		} else if ( key == OF_KEY_UP ){
-			menuPrincipal.changeLetter(true);
-		// valide le nom
-		} else if ( key == OF_KEY_RIGHT ){
-			menuPrincipal.etatMenu = 4;
-		}
-
-	/****************************** LOADING BAR *********************************************/
-	}  else if ( menuPrincipal.etatMenu == 5 ){
-
-		bool a=false,b=false,c=false,d=false;
-
-		// bouton 1
-		if ( key == OF_KEY_LEFT ){
-			a=true;
-		// bouton 2
-		} else if ( key == OF_KEY_DOWN ){
-			b=true;
-		// bouton 3
-		} else if ( key == OF_KEY_UP ){
-			c=true;
-		// bouton 4
-		} else if ( key == OF_KEY_RIGHT ){
-			d=true;
-		}
-
-		menuPrincipal.newModificateur(a,b,c,d);
-	/****************************** CHOIX MENU *********************************************/
-	} else if ( menuPrincipal.etatMenu == 6 ){
-
-		// Decale a gauche
-		if ( key == OF_KEY_LEFT ){
-			menuPrincipal.changeChoixMenu(false);
-			// decale a droite
-		} else if ( key == OF_KEY_DOWN ){
-			menuPrincipal.changeChoixMenu(true);
-			// changer la lettre ( ++ )
-		} else if ( key == OF_KEY_UP || key == OF_KEY_RIGHT ){
-			menuPrincipal.valideChoixMenu();
-		}
-
-	/****************************** PASSWORD ADMINISTRATION *********************************************/
-	} else if ( menuPrincipal.etatMenu == 8 ){
-		// bouton 1
-		if ( key == OF_KEY_LEFT ){
-			menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 1;
-			// bouton 2
-		} else if ( key == OF_KEY_DOWN ){
-			menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 2;
-			// bouton 3
-		} else if ( key == OF_KEY_UP ){
-			menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 3;
-			// bouton 4
-		} else if ( key == OF_KEY_RIGHT ){
-			menuPrincipal.sequenceHistory[menuPrincipal.indexSequence] = 4;
-		}
-		menuPrincipal.indexSequence--;
-		menuPrincipal.decoupeXBomb01++;
-		if ( menuPrincipal.decoupeXBomb01 > 8 ){ menuPrincipal.decoupeXBomb01 = 8;}
-
-
-	/****************************** CREDITS *********************************************/
-	} else if ( menuPrincipal.etatMenu == 9 ){
-
-		menuPrincipal.etatEtapeCredits++;
-		if ( menuPrincipal.etatEtapeCredits == 9 ){ 
-			myPlayer.stopSound();
-			menuPrincipal.etatMenu = 0;
-		}
-
-	/****************************** ADMINISTRATION *********************************************/
-	} else if ( menuPrincipal.etatMenu == 10 ){
-
-		// bouton 1
-		if ( key == OF_KEY_LEFT ){
-			if ( myPlayer.soundVolume < 0.04f ){
-				myPlayer.playSound("fail01",true);
-			} else {
-				myPlayer.soundVolume-=0.05f;
-			}
-			// bouton 2
-		} else if ( key == OF_KEY_DOWN ){
-			menuPrincipal.optionSelectionAdminsitration--;
-			if ( menuPrincipal.optionSelectionAdminsitration < 0 ){ menuPrincipal.optionSelectionAdminsitration = 0 ;}
-			// bouton 3
-		} else if ( key == OF_KEY_UP ){
-			menuPrincipal.optionSelectionAdminsitration++;
-			menuPrincipal.etatMenu=6;
-			// bouton 4
-		} else if ( key == OF_KEY_RIGHT ){
-			if ( myPlayer.soundVolume > 0.96f ){
-				myPlayer.playSound("fail01",true);
-			} else {
-				myPlayer.soundVolume+=0.05f;
-			}
-
-		}
-		
-
-	}
 }
 
 //--------------------------------------------------------------
